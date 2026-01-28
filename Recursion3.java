f05a07751f8edf9d6cc5c8ac1c2410b1269ba826
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-01-28 13:48:34 +0530

    //Sum of N Natural number Using Recursion

---
+++ Recursion3.java
@@ -0,0 +1,16 @@
+// Sum of n Natural Number
+public class Recursion3 {
+    public static void printSum(int i,int n,int Sum){
+    if (i==n){
+        Sum+=i;
+        System.out.println(Sum);
+        return;
+
+    }
+     Sum+=i;
+     printSum(i+1,n,Sum);
+     }
+     public static void main(String[] args) {
+    printSum(1,6,0);
+     }
+}
