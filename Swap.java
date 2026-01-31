9e67d77dd9307a1e30c7a7c9b8d69b0d0efff877
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-01-31 23:59:17 +0530

    Swap using 3rd variable

---
+++ Swap.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Swap{
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        a = sc.nextInt();
+
+        System.out.print("Enter b: ");
+        b = sc.nextInt();
+
+        // Swap using + and -
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
