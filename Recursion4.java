89bdf90d7c9ecf9d66b74a99f73b19e1c6a215e5
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-02-15 00:59:12 +0530

    Find the 1st and last occurance of an element in string

---
+++ Recursion4.java
@@ -0,0 +1,23 @@
+public class Recursion4{
+    public static int first=-1;
+    public static int last = - 1;
+    public static void findOcuurance(String str,int idx,char element ){
+        if(idx==str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return ;
+        }
+        char currChar= str.charAt(idx);
+        if(currChar==element){
+            if(first==-1){
+                first=idx;
+            }
+            last=idx;
+        }
+        findOcuurance(str, idx+1, element);
+    }
+    public static void main(String[] args) {
+        String str="abaacdaefaah";
+        findOcuurance(str, 0, 'a');
+    }
+}
