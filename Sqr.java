d3a3e99854495e909b0143967f0fb95b91bf3c61
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-02-15 11:46:44 +0530

    Implemented square area calculator using user input

--- Sqr.java
+++ Sqr.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Sqr {
     public static void main(String[] args) {
 
-        try (Scanner sc = new Scanner(System.in)) {
+        try (Scanner nw = new Scanner(System.in)) {
 
             System.out.println("Enter one Side of Square");
-            int side1 = sc.nextInt();
+            int side1 = nw.nextInt();
 
             System.out.println("Enter another Side of Square");
-            int side2 = sc.nextInt();
+            int side2 = nw.nextInt();
 
             int area = side1 * side2;
 
