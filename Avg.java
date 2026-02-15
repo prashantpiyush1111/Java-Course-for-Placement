7b1e59ef5f70756b3ca140246bb425fd67f14cf6
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-02-15 11:42:02 +0530

    Inaprogram,input3numbers:A,BandC.Youhavetooutputtheaverageofthese 3 numbers.

---
+++ Avg.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Avg {
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.println("Enter value of A");
+            int A = sc.nextInt();
+
+            System.out.println("Enter value of B");
+            int B = sc.nextInt();
+
+            System.out.println("Enter value of C");
+            int C = sc.nextInt();
+
+            int sum = A + B + C;
+            double avg = sum / 3.0;
+
+            System.out.println("Average of A, B and C is: " + avg);
+        }
+    }
+}
