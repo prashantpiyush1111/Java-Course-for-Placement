efdf4cf20ebe8290f826d0c08c0da1c0349053a9
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-03 00:08:15 +0530

    Implement printSubArray method to generate and display all possible subarrays using three nested loops.

---
+++ Array.java
@@ -0,0 +1,20 @@
+public class Array {
+    public static void printSubArray(int nums[]){
+        for(int i=0;i<nums.length;i++){
+            int start=i;
+            for(int j=i;j<nums.length;j++){
+               int end =j; 
+               for(int k=start;k<=end;k++){
+                System.out.print(nums[k]+" ");
+               }
+               System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int nums[]={2,4,6,8,10};
+        printSubArray(nums);
+
+    }
+}
