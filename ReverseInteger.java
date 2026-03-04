1c4af06c30d758ae8086ec3de7fbab465305d4a8
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-04 12:20:33 +0530

    Add Reverse Integer solution in Java

---
+++ ReverseInteger.java
@@ -0,0 +1,25 @@
+public class ReverseInteger {
+
+    public static int reverse(int x) {
+        int rev = 0;
+
+        while(x != 0){
+            int digit = x % 10;   
+            x = x / 10;           
+
+            if(rev > Integer.MAX_VALUE/10 || rev < Integer.MIN_VALUE/10){
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        int x = 1234;
+
+        System.out.println(reverse(x));   // direct call
+    }
+}
