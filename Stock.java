10aaf050ebb8ee14fb05aaa434d9c96624234c9f
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-05 00:31:57 +0530

    Add Java solution for Best Time to Buy and Sell Stock using single pass approach

---
+++ Stock.java
@@ -0,0 +1,22 @@
+class Stock {
+    public static int maxProfit(int[] prices) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } 
+            else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int price[]= {7,1,5,3,6,4};
+        System.out.println(maxProfit(price));
+
+    }
+}
