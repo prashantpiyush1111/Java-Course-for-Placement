033ea7c8609143e901b702e510604c66de07dbb8
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-04 12:21:16 +0530

    LeetCode: Reverse Pairs problem brute force solution

---
+++ ReversePAIR.java
@@ -0,0 +1,27 @@
+class ReversePAIR {
+
+    public int reversePairs(int[] nums) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if ((long) nums[i] > 2L * nums[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int nums[] = {1, 3, 2, 3, 1};
+
+        ReversePAIR obj = new ReversePAIR();  // object create
+        System.out.println(obj.reversePairs(nums));
+    }
+}
