78cffb95da67ba948c6f7625753b5dec20c6fa2f
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-06 01:47:33 +0530

    Added Bubble Sort implementation in Java with array printing method for testing sorting functionality.

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    public static void Bubble(int []nums) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=0;j<nums.length-1-i;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        } 
+    }
+    public static void printArr(int nums[]){
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int nums[] = {2,5,6,3,8,1};
+        Bubble(nums);
+        printArr(nums);
+    }
+}
