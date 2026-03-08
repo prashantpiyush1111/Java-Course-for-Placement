123207d263ac8fe511a7c7744ab13285815231dc
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-09 00:31:48 +0530

    Optimized solution to calculate sum of primary and secondary diagonals of a matrix

---
+++ DiagonalSum2.java
@@ -0,0 +1,24 @@
+//optim solution
+public class DiagonalSum2 {
+    public static int printdiagonalsum(int matrix[][]){
+        int sum=0;
+        for(int i=0;i<matrix.length;i++){
+            sum+=matrix[i][i];
+            if(i!=matrix.length-1-i){
+                sum+=matrix[i][matrix.length-1-i];
+            }
+        }
+        return sum;
+            
+        }
+        public static void main(String[]args){
+        int matrix[][]={{1,2,3},
+                         {4,5,6},
+                          {7,8,9}
+                        };
+                        System.out.println(printdiagonalsum(matrix));
+        }
+    }
+    
+
+
