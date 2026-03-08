8bda61193eeacd45935d4a7cada63ce725b37202
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-08 13:23:12 +0530

    enhanced for loop

--- Array2D2.java
+++ Array2D2.java
@@ -34,15 +34,15 @@ public class Array2D2 {
 
         System.out.println("Enter Matrix");
 
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++){
-                matrix[i][j] = sc.nextInt();
+        for (int[] matrix1 : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix1[j] = sc.nextInt();
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++){
-                System.out.print(matrix[i][j] + " ");
+        for (int[] matrix1 : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix1[j] + " ");
             }
             System.out.println();
         }
