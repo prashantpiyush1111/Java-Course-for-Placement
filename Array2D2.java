bd1421e7137a5920bcd7b7f8e04e88aadd8d8ef2
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-08 09:44:11 +0530

    Add Java program to input a 3x3 matrix, display it, and search for a key element

---
+++ Array2D2.java
@@ -0,0 +1,52 @@
+// This program takes a 3x3 matrix input from the user,
+// prints the matrix, and searches for a given key element
+// in the matrix and displays its position if found.
+
+
+import java.util.Scanner;
+
+public class Array2D2 {
+
+    public static boolean Search(int matrix[][], int key){
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for(int j = 0; j < matrix[0].length; j++){
+
+                if(matrix[i][j] == key){
+                    System.out.println("Found at cell (" + i + "," + j + ")");
+                    return true;
+                }
+
+            }
+        }
+
+        System.out.println("Key is not found");
+        return false;
+    }
+
+
+    public static void main(String args[]){
+
+        int matrix[][] = new int[3][3];
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Matrix");
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        Search(matrix,5);
+    }
+}
