d94d3f9d81f32a6861d3c57f78ef8341ada19aa2
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-08 09:43:42 +0530

    Added Java program to take input of a 3x3 matrix and print it in row and column format

---
+++ Array2D1.java
@@ -0,0 +1,28 @@
+// This program takes input of a 3x3 matrix from the user
+// and prints the matrix in row and column format
+import java.util.Scanner;
+
+public class Array2D1{
+    public static void main(String[] args) {
+        int matrix[][]=new int[3][3];
+        int n=matrix.length,m=matrix[0].length;
+        Scanner sc= new Scanner(System.in );
+        System.out.print("Enter matrix ");
+        for(int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                
+                matrix[i][j]=sc.nextInt();
+                
+                
+            }
+        }
+        //output
+        for(int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println( );
+    }
+}
+
+}
