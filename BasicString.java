9b691992d63ea63f8b20df4ebb6b94c780792a00
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-11 19:58:38 +0530

    Added basic Java String example

---
+++ BasicString.java
@@ -0,0 +1,10 @@
+public class BasicString {
+    public static void main(String[] args) {
+
+        String name = "Rahul";
+
+        System.out.println(name);          // print string
+        System.out.println(name.length()); // length of string
+        System.out.println(name.charAt(0)); // first character
+    }
+}
