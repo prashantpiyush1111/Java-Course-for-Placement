0f45b236eace7919965cbb727043a416f3fd9935
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-13 00:11:59 +0530

    Implement shortest path calculation from directional string using sqrt(x^2 + y^2)

---
+++ ShortestPath.java
@@ -0,0 +1,29 @@
+
+public class ShortestPath {
+    public static float isShortestPath(String path){
+        int x=0; int y=0;
+        for(int i=0;i<path.length();i++){
+            char dir=path.charAt(i);
+            if(dir=='E'){
+                x++;
+            }
+            else if(dir=='W'){
+                x--;
+            }
+            else if(dir=='N'){
+                y++;
+            }
+            else if(dir=='S'){
+                y--;
+            }
+
+        }
+          int x2=x*x;
+            int y2=y*y;
+            return(float) Math.sqrt(x2+y2);
+    }
+    public static void main(String[]args){
+        String path="WNEENESENNN";
+        System.out.println(isShortestPath(path));
+    }
+}
