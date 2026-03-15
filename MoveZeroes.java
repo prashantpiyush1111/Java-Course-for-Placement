9344ebd7f461dd7aeb52d89b68e41886a049614c
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-15 22:33:38 +0530

    Solved LeetCode Move Zeroes using two-pointer approach in Java.

---
+++ MoveZeroes.java
@@ -0,0 +1,31 @@
+public class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        while (j < nums.length) {
+            nums[j] = 0;
+            j++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 1, 0, 3, 12};
+
+        moveZeroes(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
