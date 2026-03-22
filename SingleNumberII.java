dbae7c2eab2f205b7aa5dd5243fc52a6be4bced3
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-22 19:54:34 +0530

    Added solution for LeetCode 260 - Single Number III

---
+++ SingleNumberII.java
@@ -0,0 +1,33 @@
+public class SingleNumberII {
+    public static int[] singleNumber(int[] nums) {
+        int xor = 0;
+
+        // Step 1: XOR of all elements
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        // Step 2: Find rightmost set bit
+        int diff = xor & (-xor);
+
+        int a = 0, b = 0;
+
+        // Step 3: Divide into 2 groups
+        for (int num : nums) {
+            if ((num & diff) == 0) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+
+        return new int[]{a, b};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,3,2,5};
+        int[] result = singleNumber(nums);
+
+        System.out.println(result[0] + " " + result[1]); // Output: 3 5
+    }
+}
