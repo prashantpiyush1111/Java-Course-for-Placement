f4852b28c7b94f14ba20cbd306807ab93de9afbd
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-04-02 16:36:25 +0530

    Add recursive method to convert digits to words

---
+++ NumberToWords.java
@@ -0,0 +1,18 @@
+public class NumberToWords{
+    static String[] words={"one","two", "three","four","five","six","seven","eight","nine","zero"};
+    public static void PrintNum(int n){
+        if(n==0){
+            return ;
+        }
+
+
+        int lastDigit= n%10;
+        PrintNum(n/10);
+        System.out.print(words[lastDigit]+" ");
+    }
+    public static void main(String[] args) {
+        int n=123;
+        PrintNum(n);
+    }
+    
+}
