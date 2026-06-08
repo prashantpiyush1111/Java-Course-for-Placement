30e0f48169b0041f70a85a5f498fd6ee11594c47
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-06-08 14:53:54 +0530

    Refactor arrange coins solution and rename class to CoinStaircase

---
+++ CoinStaircase.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class CoinStaircase {
+
+    public static int arrangeCoins(int n) {
+        return (int) ((Math.sqrt(1 + 8L * n) - 1) / 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(arrangeCoins(n));
+
+        sc.close();
+    }
+}
